28360e60def7d2e362f6ff08751ac1fa4f1d8799
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2017-09-23 21:42:05 +0200

    Renamed main controller.

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QAware.OSS.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public virtual IActionResult Get()
+        {
+            return this.Ok("Hello BASTA! 2017");
+        }
+    }
+}
