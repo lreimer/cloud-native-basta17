45981142a9d2d018f55bf1f93a81c8bb33a064e2
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2017-09-24 09:15:01 +0200

    Added server URLs parameter.

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace QAware.OSS
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls(GetServerUrls(args))
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
@@ -21,5 +22,18 @@ namespace QAware.OSS
 
             host.Run();
         }
+
+		private static string[] GetServerUrls(string[] args)
+		{
+			List<string> urls = new List<string>();
+			for (int i = 0; i < args.Length; i++)
+			{
+				if ("--server.urls".Equals(args[i], StringComparison.OrdinalIgnoreCase))
+				{
+					urls.Add(args[i + 1]);
+				}
+			}
+			return urls.ToArray();
+		}
     }
 }
